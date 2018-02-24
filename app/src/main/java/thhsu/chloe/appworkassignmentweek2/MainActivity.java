3bdf42b5bec9c297b06bf2fd52920e628d0e4a04
Chloe Hsu <vivi831018@hotmail.com>
2018-02-24 20:29:08 +0800

    Added function for displaying calculator symbol

--- app/src/main/java/thhsu/chloe/appworkassignmentweek2/MainActivity.java
+++ app/src/main/java/thhsu/chloe/appworkassignmentweek2/MainActivity.java
@@ -1,13 +1,35 @@
 package thhsu.chloe.appworkassignmentweek2;
 
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupOnClick((ImageButton) findViewById(R.id.button1),"1");
+        setupOnClick((ImageButton) findViewById(R.id.button2),"2");
+        setupOnClick((ImageButton) findViewById(R.id.buttonPlus), "+");
+        setupOnClick((ImageButton) findViewById(R.id.buttonEquals), "=");
+
+    }
+
+    private void setupOnClick(ImageButton b, final String calSymbol) {
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView mathInput = (TextView) findViewById(R.id.mathInput);
+                mathInput.setText(mathInput.getText()+ calSymbol);
+            }
+        });
     }
 }
